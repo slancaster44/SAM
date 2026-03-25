d0a14ff473f7479639a945d1c92cd75881170db7
Stanley <lancasterharp@gmail.com>
2026-03-24 20:56:02 -0400

    some work done

--- Software/Assembler/Parser.pas
+++ Software/Assembler/Parser.pas
@@ -1,19 +1,78 @@
 
 type
-  AstKind = (ORG, DEF);
-  Ast = record
-
-
-    case kind : AstKind of
-      ORG : (location : cardinal);
-      DEF : (labelName : pString);
+  SyntaxTree = record
+    child : ^SyntaxTree;
+    tok : Token;
   end;
-  pAst = ^Ast;
+  pSyntaxTree = ^SyntaxTree;
 
-  ParseFn = function (arena : pArena; lex : pLexer) : pAst;
+  SyntaxRule = record
+    next : ^SyntaxRule;
+    match : TokenKind;
+  end;
+  pSyntaxRule = ^SyntaxRule;
+
+  RuleList = record
+    next : ^RuleList;
+    rule : SyntaxRule;
+  end;
+  pRuleList = ^RuleList;
+
   Parser = record
-    fn : ParseFn;
-    next : ^Parser;
-    alternate : ^Parser;
+    lex : pLexer;
+    stmtArena : pArena;
+    rules : pRuleList;
   end;
   pParser = ^Parser;
+
+function AppendRule(a : pArena; kind : TokenKind; head : pSyntaxRule) : pSyntaxRule;
+var
+  newRule : pSyntaxRule;
+begin
+  newRule := ArenaAllocate(a, sizeof(SyntaxRule));
+  newRule^.next := nil;
+  newRule^.match := kind;
+
+  if head = nil then
+    begin
+      AppendRule := newRule;
+    end
+  else
+    begin
+      while (head^.next <> nil) do
+        head := head^.next;
+      
+      head^.next := newRule;
+      AppendRule := newRule;
+    end;
+
+end;
+
+function SyntaxMatch(a : pArena; lexer : pLexer; rule : pSyntaxRule) : pSyntaxTree;
+var
+  tok : Token;
+  output : pSyntaxTree;
+begin
+  output := nil;
+  tok := NextToken(lexer);
+  if tok.kind = rule^.match then
+    begin
+      output := ArenaAllocate(a, sizeof(SyntaxTree));
+      output^.tok := tok;
+    end;
+
+  if rule^.next <> nil then
+    begin
+      output^.child := SyntaxMatch(a, lexer, rule^.next);
+      if output^.child = nil then output := nil;
+    end;
+  
+  SyntaxMatch := output;
+end;
+
+function NewParser(a : pArena; lexer : pLexer) : pParser;
+var
+  output : pParser;
+begin
+  output := ArenaAllocate(a, sizeof(Parser));
+end;
